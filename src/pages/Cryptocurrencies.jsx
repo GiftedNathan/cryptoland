bf5d7f40e076f4550ccad22430c18494357c917c
giftedNathan <eje.nathaniel@gmail.com>
2022-11-08 16:03:50 +0100

    Updated Home page; finshed the UI for stats section and added the UI for cryptocurencies page

--- src/pages/Cryptocurrencies.jsx
+++ src/pages/Cryptocurrencies.jsx
@@ -2,7 +2,71 @@ import React from 'react'
 
 const Cryptocurrencies = () => {
   return (
-    <div>Cryptocurrencies</div>
+    
+
+    <div className="container px-4 py-5" id="icon-grid">
+      <h2 className="pb-2 border-bottom">Cryptocurrencies</h2>
+
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ico</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
+          </div>
+        </div>
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ic</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
+          </div>
+        </div>
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ic</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
+          </div>
+        </div>
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ic</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
+          </div>
+        </div>
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ic</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
+          </div>
+        </div>
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ic</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
+          </div>
+        </div>
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ic</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
+          </div>
+        </div>
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ic</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className='container pt-5 pb-5' >
 
-      <div className="jumbotron py-5">
+      <div className="jumbotron ">
         <h1 className="display-1">Hello, world!</h1>
         <p className="lead">This is a one stop home for all information about crytocurencies. </p>
         <hr className="my-4" />
@@ -37,28 +37,48 @@ const Home = () => {
       </div>
 
 
-      <div className="row bg-light mt-5 py-5 px-3">
-        <h1 className="display-6 lead">Top Cryptocurrencies</h1>
-     
-        <div class="row align-items-md-stretch mt-3">
-          
-          <div className="col-md-4 mt-3 ">
-            <div className="h-100 p-3  border rounded-3">
-              <h3>Special title</h3>
-              <p>43,232 </p>            
-            </div>
+      <div className="container px-4 py-5" id="icon-grid">
+      <h2 className="pb-2 border-bottom">Top Cryptocurrencies</h2>
+
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ico</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
           </div>
-          
-          <div className="col-md-4 mt-3 ">
-            <div className="h-100 p-3  border rounded-3">
-              <h3>Special title</h3>
-              <p>43,232 </p>            
-            </div>
+        </div>
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ic</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
+          </div>
+        </div>
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ic</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
+          </div>
+        </div>
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ic</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
+          </div>
+        </div>
+        <div className="col d-flex align-items-start">
+          <span className="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">ic</span>
+          <div>
+            <h3 className="fw-bold mb-0 fs-4">Featured title</h3>
+            <p>Paragraph of text beneath the heading to explain the heading.</p>
           </div>
         </div>
         
-        <h3 className="display-7 lead mt-5">More statistics</h3>
       </div>
+    </div>
 
 
       {/* <ListGroup as="ol" className="d-flex flex-wrap " numbered>
